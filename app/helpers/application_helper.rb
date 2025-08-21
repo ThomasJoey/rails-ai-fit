36df94ba40978188bf1178de2fb9755b68a75976
mihran benslama <mihranx@yahoo.fr>
2025-08-21 16:22:34 +0200

    navbar2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def body_class_for_page
+    classes = []
+
+    if devise_controller?
+      classes << "devise-controller"
+      classes << "devise-#{controller_name}"
+      classes << "devise-#{controller_name}-#{action_name}"
+    else
+      classes << "main-app"
+      classes << "#{controller_name}"
+      classes << "#{controller_name}-#{action_name}"
+    end
+
+    classes.join(' ')
+  end
+
+  def main_content_class
+    if devise_controller?
+      "devise-content"
+    else
+      "main-content"
+    end
+  end
 end
