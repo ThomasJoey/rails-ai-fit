5c7776bfa8275bcf298b77a17ee6028aeb69fbb5
mihran benslama <mihranx@yahoo.fr>
2025-08-27 15:35:41 +0200

    push some missing commits

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -53,10 +53,10 @@ class ProfilesController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :bio, :avatar, :location, sports: [])
+    params.require(:user).permit(:first_name, :last_name, :bio, :sexe, :avatar, :location, sports: [])
   end
 
   def create
-    
+
   end
 end
